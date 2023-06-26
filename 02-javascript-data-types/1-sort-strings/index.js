dbd96387098e1f136ce4e8dc9be1bb2a14826455
Ivan Pautov <ivan@machine.local>
2023-06-26 05:10:06 -0700

    Implemented multilingual string sorting

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,43 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
 
+export function sortStrings(arr, param = 'asc') {
+    
+  const LOCALE_OPTIONS = {
+    caseFirst: 'upper',
+  };
+
+  const sortedLanguages = arr.reduce(reduceStringsByLanguage, {});
+
+  const sortedStrings = Object.entries(sortedLanguages)
+                          .map(sortStrings)
+                          .reduce((arrays, array) => arrays.concat(array), []);
+
+  return param === 'asc' ? sortedStrings : sortedStrings.reverse();
+
+  // helper functions
+    
+  function sortStrings([language, strings]) {
+    return strings.sort((a, b) => a.localeCompare(b, language, LOCALE_OPTIONS));
+  }
+
+  function reduceStringsByLanguage(cache, currentString) {
+    const language = detectLanguage(currentString[0]);
+
+    if (!cache[language]) {
+      cache[language] = [];
+    }
+
+    cache[language].push(currentString);
+
+    return cache;
+  }
+
+  function detectLanguage(char) {
+    let charCode = char.charCodeAt();
+
+    return ((charCode >= 1040 && charCode <= 1103) || charCode === 1025 || charCode === 1105) ? 'ru' : 'en';
+  }
+    
 }
